640e186efc9f7a00df2986e973e83646f17773a5
hsk1955 <rhdwn1835@naver.com>
2022-05-24 10:57:09 +0900

    First commit

---
+++ app/src/main/java/com/example/cody_c_pagecodystyle6/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.cody_c_pagecodystyle6;
+
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomAdapter adapter;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new CustomAdapter();
+        listView = (ListView) findViewById(R.id.listView);
+
+        setData();
+
+        listView.setAdapter(adapter);
+
+
+    }
+
+
+    // 보통 ListView는 통신을 통해 가져온 데이터를 보여줍니다.
+    // arrResId, titles, contents를 서버에서 가져온 데이터라고 생각하시면 됩니다.
+    private void setData() {
+        TypedArray arrResId = getResources().obtainTypedArray(R.array.resId);
+        String[] contents = getResources().getStringArray(R.array.content);
+
+        for (int i = 0; i < arrResId.length(); i++) {
+            CustomDTO dto = new CustomDTO();
+            dto.setResId(arrResId.getResourceId(i, 0));
+            dto.setContent(contents[i]);
+
+            adapter.addItem(dto);
+        }
+    }
+}
